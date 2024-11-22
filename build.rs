98772de7e1ed63fc1def58fe7162a63bd66c025a
Douglas Dwyer <dougmdwyer@hotmail.com>
2024-11-22 16:52:30 -0500

    Add support for statically/dynamically linked CRT and remove windows_core dependency

--- build.rs
+++ build.rs
@@ -7,10 +7,10 @@ use std::process::*;
 /// Downloads and links the static DXC binary.
 fn main() {
     let target = var("TARGET").expect("Failed to get target triple");
-    
+
     let out_dir = PathBuf::from(var("OUT_DIR").expect("Failed to get output directory"));
     let file_path = out_dir.join("machdxcompiler.tar.gz");
-    download_url(&get_target_url(&target), &file_path);
+    download_url(&get_target_url(&target, static_crt()), &file_path);
     extract_tar_gz(&file_path, &out_dir);
 
     println!("cargo:rustc-link-lib=static=machdxcompiler");
@@ -18,19 +18,20 @@ fn main() {
 }
 
 /// Gets the URL from which the DXC binary should be downloaded.
-fn get_target_url(target: &str) -> String {
-    const BASE_URL: &str = "https://github.com/hexops/mach-dxcompiler/releases/download/2023.12.14%2B0b7073b.1/";
+fn get_target_url(target: &str, static_crt: bool) -> String {
+    const BASE_URL: &str = "https://github.com/DouglasDwyer/mach-dxcompiler/releases/download/2024.11.22%2Bbfceb9a.1/";
 
-    BASE_URL.to_string() + match target {
-        "x86_64-pc-windows-msvc" => "x86_64-windows-msvc_ReleaseFast_lib.tar.gz",
-        "x86_64-pc-windows-gnu" => "x86_64-windows-gnu_ReleaseFast_lib.tar.gz",
+    BASE_URL.to_string() + match (target, static_crt) {
+        ("x86_64-pc-windows-msvc", false) => "x86_64-windows-msvc_ReleaseFast_Dynamic_lib.tar.gz",
+        ("x86_64-pc-windows-msvc", true) => "x86_64-windows-msvc_ReleaseFast_lib.tar.gz",
+        ("x86_64-pc-windows-gnu", _) => "x86_64-windows-gnu_ReleaseFast_lib.tar.gz",
         _ => panic!("Unsupported target '{target}' for mach-dxcompiler")
     }
 }
 
 /// Downloads the provided URL to a file.
 fn download_url(url: &str, file_path: &Path) {
-    Command::new("curl")
+    let result = Command::new("curl")
         .arg("--location")
         .arg("-o")
         .arg(file_path)
@@ -39,12 +40,15 @@ fn download_url(url: &str, file_path: &Path) {
         .expect("Failed to start Curl to download DXC binary")
         .wait()
         .expect("Failed to download DXC binary");
+    if !result.success() {
+        panic!("{result}");
+    }
 }
 
 /// Extracts the file at the provided path as a `.tar.gz` file.
 /// The contents are extracted to the current directory.
 fn extract_tar_gz(path: &Path, output_dir: &Path) {
-    Command::new("tar")
+    let result = Command::new("tar")
         .current_dir(output_dir)
         .arg("-xzf")
         .arg(path)
@@ -52,4 +56,14 @@ fn extract_tar_gz(path: &Path, output_dir: &Path) {
         .expect("Failed to start Tar to extract DXC binary")
         .wait()
         .expect("Failed to extract DXC binary");
+    if !result.success() {
+        panic!("{result}");
+    }
+}
+
+/// Determines whether the CRT is being statically or dynamically linked.
+fn static_crt() -> bool {
+    var("CARGO_ENCODED_RUSTFLAGS")
+        .unwrap_or_default()
+        .contains("target-feature=+crt-static")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,16 @@
 
 use std::ffi::*;
 use std::sync::*;
-use windows_core::*;
 
 /// Invokes the native [`DxcCreateInstance`](https://learn.microsoft.com/en-us/windows/win32/api/dxcapi/nf-dxcapi-dxccreateinstance)
 /// funcion. Creates a new uninitialized object of the specified class.
 /// 
 /// # Safety
 /// 
-/// `rclsid` and `riid` must point to valid Windows [`GUID`]s. `ppv` must point to a writable
+/// `rclsid` and `riid` must point to valid Windows `GUID`s. `ppv` must point to a writable
 /// `*mut c_void` instance.
 #[allow(non_snake_case)]
-pub unsafe extern "system" fn DxcCreateInstance(rclsid: *const GUID, riid: *const GUID, ppv: *mut *mut c_void) -> HRESULT {
+pub unsafe extern "system" fn DxcCreateInstance(rclsid: *const c_void, riid: *const c_void, ppv: *mut *mut c_void) -> c_long {
     /// Lock which ensures that the dxcompiler library has been initialized before use.
     static MAIN_INITIALIZER: Once = Once::new();
     MAIN_INITIALIZER.call_once(|| unsafe { bindings::MachDxcompilerInvokeDllMain() });
@@ -26,7 +25,7 @@ mod bindings {
 
     extern "system" {
         /// Calls the [`DxcCreateInstance`](https://learn.microsoft.com/en-us/windows/win32/api/dxcapi/nf-dxcapi-dxccreateinstance) function.
-        pub fn DxcCreateInstance(rclsid: *const GUID, riid: *const GUID, ppv: *mut *mut c_void) -> HRESULT;
+        pub fn DxcCreateInstance(rclsid: *const c_void, riid: *const c_void, ppv: *mut *mut c_void) -> c_long;
 
         /// Invokes the [`DllMain`](https://learn.microsoft.com/en-us/windows/win32/dlls/dllmain) entrypoint for the dxcompiler library.
         pub fn MachDxcompilerInvokeDllMain();
